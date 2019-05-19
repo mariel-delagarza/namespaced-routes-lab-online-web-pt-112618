f7f2994b8d2b4a4b4f1d454f7f876e6937a112fb
mariel-delagarza <mariel.code@gmail.com>
2019-05-19 18:38:31 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists do 
+    resources :song, only: [:index, :show]
+  end
+  resources :songs 
+
+  namespace :admin do
+    resources :preferences, only: [:index, :update]
+  end
 end
